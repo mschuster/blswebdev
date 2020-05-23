47344aa90903e71f03035da07b38bd90f2bb937c
Martin Schuster <martin.schuster1988@gmail.com>
2020-05-23 23:20:55 +0200

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,23 @@ const AboutPage = () => (
       <SEO title="About" />
       <h1>Über uns</h1>
       <p>
-      Ut varius tincidunt libero. Nunc nulla. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Sed cursus turpis vitae tortor. Morbi ac felis.
-
-Fusce a quam. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Etiam iaculis nunc ac metus.
-
-Pellentesque auctor neque nec urna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus a est. Sed lectus. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi.
-
-Nunc sed turpis. Curabitur ullamcorper ultricies nisi. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Curabitur at lacus ac velit ornare lobortis. Aenean ut eros et nisl sagittis vestibulum.
-
-Aenean ut eros et nisl sagittis vestibulum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Curabitur nisi. Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Praesent egestas neque eu enim.       
+      Wir sind ein Team von drei Studenten an der Hochschule Mittweida und bieten ihnen 
       </p>
+      <div class='teambox'>
+        <div class='teambox-item'>
+          <img src={'../../hiclipart.com.png'} alt="Profile Pciture"></img>
+          Susan Labude (Desing)
+        </div>
+        <div class='teambox-item'>
+          <img src={'../../hiclipart.com.png'} alt="Profile Pciture"></img>
+          Dominik Breck (Tech)
+          </div>
+        
+        <div class='teambox-item'>
+         <img src={'../../hiclipart.com.png'} alt="Profile Pciture"></img>
+         Martin Schuster (Tech)
+        </div>
+      </div>
     </Layout>
   )
   
