733d5c267b418f86b14bd44ca760cb9b21e9ab9a
vXDarkestNightXv <40502315+vXDarkestNightXv@users.noreply.github.com>
2020-05-31 13:20:54 +0200

    Update contact.js
    Created contact form base with the netify service

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,6 +17,12 @@ Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce commodo al
 Pellentesque commodo eros a enim. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.
 
 Integer tincidunt. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus.
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input name="name" placeholder="Name" type="text"/><br/><br/>
+        <input name="email" placeholder="Ihre E-mail Adresse" type="email"/><br/><br/>
+        <textarea name="message"/><br/><br/>
+        <button>Senden</button>
+      </form>
     </p>
   </Layout>
 )
