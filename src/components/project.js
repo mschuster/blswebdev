7f7b2f6a217b4a382e4bd5f9337b49e6dad21256
Martin Schuster <37813025+mschuster@users.noreply.github.com>
2020-06-07 09:20:37 +0200

    Fix Images

--- src/components/project.js
+++ src/components/project.js
@@ -3,15 +3,15 @@ import { Link } from 'gatsby';
 import Image from 'gatsby-image';
 
 const Project = ({ title, description, url, imageData }) => (
-    <div className="project">
+    <div className="project" style={{color: 'white'}}>
         <h1>{title}</h1>
         <Image fluid={imageData} alt={title}/>
         <p>{description}</p>
         <p>
-            <a href={url}>Webseite besuchen &rarr;</a>
+            <a href={url} style={{textDecoration:'none'}}>Webseite besuchen &rarr;</a>
         </p>
         <p>
-            <Link to="/portfolio">&larr; Portfolio Ansicht aufrufen</Link>
+            <Link to="/portfolio" style={{textDecoration:'none'}}>&larr; Portfolio Ansicht aufrufen</Link>
         </p>
     </div>
 );
